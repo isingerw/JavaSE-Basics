d1867252f54cf90e6b0e773d4c7d2a6a388791b9
zhangsinger <zhangsingerw@gmail.com>
2021-05-29 04:12:23 +0800

    update

--- 6. JavaSE综合小练习/src/com/singerw/dayangdaan/Menu.java
+++ 6. JavaSE综合小练习/src/com/singerw/dayangdaan/Menu.java
@@ -1,11 +1,26 @@
 package com.singerw.dayangdaan;
 
+/**
+ * @ClassName: Menu
+ * @Description: //TODO ¿‡√Ë ˆ
+ * @Author: CodeSleep
+ * @Date: 2021-05-29 3:55
+ * @Version: 1.0
+ */
 public class Menu {
 
     public void login(){
 
     }
 
+
+    /**
+    * @Description: //TODO ∑Ω∑®√Ë ˆ
+    * @param: []
+    * @return: void
+    * @Author: CodeSleep
+    * @Date: 2021-05-29 4:01
+    */
     public void showMainMenu(){
         System.out.println("**********************************");
         System.out.println("1 ≥ÈΩ±");
--- 6. JavaSE综合小练习/src/com/singerw/dayangdaan/User.java
+++ 6. JavaSE综合小练习/src/com/singerw/dayangdaan/User.java
@@ -1,14 +1,15 @@
 package com.singerw.dayangdaan;
 
 /**
-* @ClassName: User
-* @Description: TODO(这里用一句话描述这个类的作用)
-* @Author 张欣
-* @Date 2021-05-27 9:35
-*/
+ * @ClassName: User
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @Author 张欣
+ * @Date 2021-05-27 9:35
+ */
 public class User {
     private String username;
     private String password;
+
 
     public String getUsername() {
         return username;
@@ -18,6 +19,7 @@ public class User {
         this.username = username;
     }
 
+
     public String getPassword() {
         return password;
     }
@@ -25,7 +27,6 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
-
 
 
     public User(String username, String password) {
@@ -43,15 +44,16 @@ public class User {
 
     /**
      * 登录比较用户名和密码
+     *
      * @param user
      * @return
      */
-    public boolean login(User user){
+    public boolean login(User user) {
         // 比较当前类的对象 和 传递进来的user的用户名和密码是否相等
-        if (this == user){
+        if (this == user) {
             return true;
         }
-        if (this.username.equals(user.getUsername())){
+        if (this.username.equals(user.getUsername())) {
             return false;
         }
         return !this.password.equals(user.getPassword());
